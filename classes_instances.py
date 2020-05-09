23cb8b54fe0c404d0d8088cc2f52b5e5d7a1d6b7
Zack Bunch <zackbunch@MacBook-Pro.local>
2020-05-09 13:32:11 -0400

    class methods and static methods

--- classes_instances.py
+++ classes_instances.py
@@ -18,13 +18,31 @@ class Employee:
 
 
     def applyRaise(self):
-        self.pay = int(self.pay * Employee.raise_amount)
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls,emp_str):
+        first,last,pay = emp_str.split('-')
+        return cls(first,last,pay)
 
 
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
-print(Employee.num_of_employees)
 emp_1 = Employee('Zack','Bunch',95000)
 emp_2 = Employee('Will','Rochelle',96000)
 
-print(Employee.num_of_employees)
+# static methods
+
+import datetime
+my_date = datetime.date(2020,5,11)
+
+print(Employee.is_workday(my_date))
